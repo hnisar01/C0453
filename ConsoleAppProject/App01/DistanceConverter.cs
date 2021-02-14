805afa24fc8c4133a42a9918260cf11975510fb6
hnisar01 <72013214+hnisar01@users.noreply.github.com>
2021-02-14 11:37:20 +0000

    Added few methods

--- ConsoleAppProject/App01/DistanceConverter.cs
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -2,12 +2,48 @@
 {
     /// <summary>
     /// Please describe the main features of this App
-    /// </summary>
-    /// <author>
+    /// This app converts distances by other distances
+    /// Hassan Nisar
     /// Student Name version 0.1
-    /// </author>
+    /// Hassan Nisar
     public class DistanceConverter
     {
+        private double miles;
+       
+        private double feet;
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        public void Run()
+        {
+        
+        }
+        
+        /// <summary>
+        /// Prompt the user to enter the distance in miles
+        /// Input the miles as a double number
+        /// </summary>
+        private void InputMiles()
+        {
 
+        }
+        
+        /// <summary>
+        /// Prompt the user to enter the distance in feet
+        /// Input the miles as a double number
+        /// </summary> 
+        private void CalculateFeet()
+        { 
+
+        }
+
+        /// <summary>
+        /// Prompt the app to output the distance in feet
+        /// </summary>
+        private void OutputFeet()
+        {
+        
+        }
     }
 }
