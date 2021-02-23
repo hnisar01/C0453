9f529bfa6a28f4e65827e5ef33625f95d348c6b5
hnisar01 <72013214+hnisar01@users.noreply.github.com>
2021-02-23 11:56:07 +0000

    started app02

--- ConsoleAppProject/App02/BMI.cs
+++ ConsoleAppProject/App02/BMI.cs
@@ -1,12 +1,37 @@
 ﻿namespace ConsoleAppProject.App02
 {
     /// <summary>
-    /// Please describe the main features of this App
+    /// To allow the user to check their overall health by BMI
     /// </summary>
     /// <author>
-    /// Student Name version 0.1
+    /// Hassan Nisar version 0.1
     /// </author>
     public class BMI
     {
+    }
+
+    private void OutputHeading()
+    {
+        Console.WriteLine();
+        Console.WriteLine(" -------------------------");
+        Console.WriteLine("   Body Mass Index Calculator      ");
+        Console.WriteLine("    By Hassan Nisar       ");
+        Console.WriteLine(" -------------------------");
+    }
+
+    public void Run()
+    {
+        fromUnit = SelectUnit(" Please select the from distance unit > ");
+        toUnit = SelectUnit(" Please select  the to distance unit");
+
+        OutputHeading();
+
+        Console.WriteLine($"\n Converting {fromUnit} to {toUnit}");
+
+        fromDistance = InputDistance($"Please enter the number of {fromUnit}  > ");
+
+        CalculatetoDistance();
+
+        OutputtoDistance();
     }
 }
